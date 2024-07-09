30a9bab336c87d75c31e9114a2707bacfa6d25ef
maheshryali1122 <159117395+maheshryali1122@users.noreply.github.com>
2024-07-09 19:50:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import useFetch from "hooks/useFetch";
 
 
 export default function App() {
-  const {loading, error,data} = useFetch('http://34.221.6.26:1337/api/blogs?populate=*');
+  const {loading, error,data} = useFetch('http://35.88.228.138:32768/api/blogs?populate=*');
   if(loading) return <p>Loading...</p>
   if(error) return console.log(error)
 
